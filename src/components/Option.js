c0a426153a9ea1ed8922bbf0f05f5c7f90bc14b3
SohaibHaseeb7759 <sohaibhaseeb775916@gmail.com>
2024-01-28 17:08:06 +0500

    first commit

---
+++ src/components/Option.js
@@ -0,0 +1,27 @@
+function Option({ question, dispatch, answer }) {
+  const hasAnswered = answer !== null;
+
+  return (
+    <div className="options">
+      {question.options.map((option, index) => (
+        <button
+          className={`btn btn-option 
+        ${index === answer ? "answer" : console.log(answer)} ${
+            hasAnswered
+              ? index === question.correctOption
+                ? "correct"
+                : "wrong"
+              : ""
+          }`}
+          disabled={hasAnswered}
+          key={option}
+          onClick={() => dispatch({ type: "newAnswer", payload: index })}
+        >
+          {option}
+        </button>
+      ))}
+    </div>
+  );
+}
+
+export default Option;
---
+++ src/components/Question.js
@@ -0,0 +1,15 @@
+import Option from "./Option";
+
+function Question({ question, dispatch, answer }) {
+  console.log(question);
+
+  return (
+    <div>
+      <h3>{question.question}</h3>
+      {console.log(dispatch)}
+      <Option question={question} dispatch={dispatch} answer={answer} />
+    </div>
+  );
+}
+
+export default Question;
